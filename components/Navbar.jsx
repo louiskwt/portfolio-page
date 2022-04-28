a4c64e12439a2deca674c8f0ed86819902f30aa0
Louis Tsang <kawingt2@gmail.com>
2022-04-28 13:58:54 +0800

    implemented dark theme and decoupled Navbar css

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,15 +1,17 @@
-import { UilEstate, UilUser, UilScenery, UilApps, UilTimes } from '@iconscout/react-unicons'
-import { useState } from 'react'
+import { UilEstate, UilUser, UilScenery, UilApps, UilTimes, UilMoon, UilSun } from '@iconscout/react-unicons'
+import { useState, useEffect } from 'react'
 import Link from 'next/link';
 import { useRouter } from 'next/router';
+import styles from '../styles/Navbar.module.css'
 
 
 
 const Navbar = () => {
+	// Menu State
 	const [showMenu, setShowMenu] = useState(false)
 	// Handle Show menu
 	const toggleMenu = () => {
-		console.log('fired')
+		// console.log('fired')
 		setShowMenu(!showMenu)
 	}
 	
@@ -17,29 +19,56 @@ const Navbar = () => {
 	const router = useRouter()
 	const path = router.pathname
 
+	// DARK / LIGHT MODE STATE
+	const [darkMode, setDarkMode] = useState(false)
+
+	useEffect(() => {
+		const storedMode = localStorage.getItem('dark-mode');
+		if(storedMode === 'true') {
+			document.body.classList.add('dark-theme');
+			setDarkMode(!darkMode)
+		}
+		return () => {
+			document.body.classList.add('dark-theme');
+		}
+	}, [])
+
+	// Handle light / dark mode toggle
+	const changeMode = () => {
+		if(!darkMode) {
+			document.body.classList.add('dark-theme');
+		} else {
+			document.body.classList.remove('dark-theme')
+		}
+		// Change state
+		setDarkMode(!darkMode)
+		// Store the mode in local storage
+		localStorage.setItem('dark-mode', !darkMode)
+	}
+
 	return (
-		<header className='header' id="header">
-			<nav className="nav container">
+		<header className='header'>
+			<nav className={`${styles.nav} container`}>
 				<Link href='/'>
 					<a className="nav__logo">
 						Louis T
 					</a>
 				</Link>
 				{/* Menu */}
-				<div className={"nav__menu " + (showMenu && "show-menu")} id="nav-menu">
-					<ul className="nav__list grid">
+				<div className={`${styles.nav__menu} ` + (showMenu && `${styles.show__menu}`)} id="nav-menu">
+					<ul className={`${styles.nav__list} grid`}>
 						{/* Nav item with active link */}
 						<li className="nav__item">
 							<Link href="/" >
-								<a href="" className={"nav__link " + (path === '/' && 'active-link' )} onClick={toggleMenu}>
-									<UilEstate className="nav__icon" />
+								<a href="" className={`${styles.nav__link} ` + (path === '/' ? `${styles.active__link}` : '' )} onClick={toggleMenu}>
+									<UilEstate className={`${styles.nav__icon}`} />
 									Home
 								</a>
 							</Link>
 						</li>
 						<li className="nav__item">
 							<Link href="/about">
-								<a href="" className={"nav__link " + (path === '/about' && 'active-link')} onClick={toggleMenu}>
+								<a href="" className={`${styles.nav__link} ` + (path === '/about' ? `${styles.active__link}` : '')} onClick={toggleMenu}>
 									<UilUser className="nav__icon" />
 									About
 								</a>
@@ -47,8 +76,8 @@ const Navbar = () => {
 						</li>
 						<li className="nav__item">
 							<Link href="/portfolio">
-								<a href="" className={"nav__link " + (path === '/portfolio' && 'active-link')} onClick={toggleMenu}>
-									<UilScenery className="nav__icon" />
+								<a href="" className={`${styles.nav__link} ` + (path === '/portfolio' ? `${styles.active__link}` : '')} onClick={toggleMenu}>
+									<UilScenery className={`${styles.nav__icon}`} />
 									Portfolio
 								</a>
 							</Link>
@@ -56,12 +85,16 @@ const Navbar = () => {
 		
 					</ul>
 					{/* Close Icon */}
-					<UilTimes className="nav__close" onClick={toggleMenu} />
+					<UilTimes className={styles.nav__close} onClick={toggleMenu} />
 				</div>
 				{/* Nav buttons */}
-				<div className="nav-btns">
+				<div className={styles.nav__btns}>
+					<div className={styles.nav__toggle}>
+						{darkMode ? <UilSun className={styles.change__theme} onClick={changeMode} /> : <UilMoon className={styles.change__theme} onClick={changeMode} />}
+					</div>
+				
 					{/* Toggle Menu */}
-					<div className="nav__toggle" id="nav-toggle">
+					<div className={styles.nav__toggle} >
 						<UilApps onClick={toggleMenu} />
 					</div>
 				</div>
