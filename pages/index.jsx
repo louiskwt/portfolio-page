6ed22222c95b64af50e5a55dfad5adbe53b651c9
Louis Tsang <kawingt2@gmail.com>
2024-10-14 22:47:41 +0800

    clean up and start anew

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,7 +2,7 @@ import Meta from "../components/Meta";
 
 const META = {
   title: "Louis Tsang - Software Engineer",
-  desc: "A Professional Software Engineer based in Hong Kong specialised in developing web applications and web services. Always strive to deliver code that can bring joy to end users and make a impact.",
+  desc: "A software engineer from Hong Kong",
   canonical: "https://www.louistsang.me/",
 };
 
@@ -10,25 +10,19 @@ export default function Home() {
   return (
     <>
       <Meta title={META.title} desc={META.desc} canonical={META.canonical} />
-      <main id="container">
-        <nav>
-          <ul>
-            <li>
-              <a class="" href="/posts/">
-                <i class="fa-li fa  fa-lg"></i>
-                <span>Posts</span>
-              </a>
-            </li>
-
-            <li>
-              <a class="" href="/about/">
-                <i class="fa-li fa  fa-lg"></i>
-                <span>About Hugo</span>
-              </a>
-            </li>
-          </ul>
-        </nav>
-        <h1>Hello</h1>
+      <main>
+        <h1>Hey, this's Louis</h1>
+        <p>Before I became a developer, I used to teach English full time in Hong Kong for 3 years.</p>
+        <p>After working at Retykle for 2+ years, I decided to take a study break as I began to experience a lack of growth and feel complacent.</p>
+        <p>Meanwhile, given the advance in AI and the rise of LLM, the whole world is undergoing a rapid change and yet no one knows where this trend will lead us.</p>
+        <p>Because of that I'm now committed to dive deeper into CS and ML/AI so that I can move forward.</p>
+        {/* link */}
+        <p id="links">
+          👋 Follow me on 𝕏: <a href="https://x.com/kawingt2">@EurekaLabsAI</a>
+          <br />
+          💻 And/or GitHub: <a href="https://github.com/louiskwt">EurekaLabsAI</a>
+          <br />
+        </p>
       </main>
     </>
   );
