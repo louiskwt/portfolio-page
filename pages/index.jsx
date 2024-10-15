c1934970c77b3906c0914de7577e6c6732dedc0d
Louis Tsang <kawingt2@gmail.com>
2024-10-15 16:31:58 +0800

    added personal bio to home page to replace about

--- pages/index.jsx
+++ pages/index.jsx
@@ -37,9 +37,17 @@ export default function Home() {
                 <br />
                 Right now, I'm fully committed to dive deeper into computer science and catch up with the current trend of AI and ML. If you want to connect feel free to find me on social media or check my github as well. I'm also open to work on a project if I'm a good fit and the problems that you or your team is trying to solve interest me.
               </p>
+              <hr />
+              <h1 className={styles.home__title}>Bio</h1>
+
+              <div className={styles.home__description}>Before I became a developer, I worked as an English teacher for more than 3 years. During the Covid-19 pandemic, I decided to step out of my comfort zone and learned something new by teaching myself how to program through online resoucres</div>
+              <div className={styles.home__description}>Surprisingly, learning how to program somehow drove me towards building porojects for my students to use in my class, and the joy of seeing how my projects were used and enjoyed by my students got me hooked.</div>
+              <div className={styles.home__description}>That's why in 2022, I made an unexpected pivot: getting a job as a full-stack developer at a startup called Retykle. And after working at Retykle for 2+ years, I decdied to take a study break and allowed myself to dive deeper into the study of computer science.</div>
+              <div className={styles.home__description}>Meanwhile, I'm also working as a freelancer and building my own projects.</div>
             </div>
           </div>
         </section>
+        {/* About */}
       </main>
     </>
   );
